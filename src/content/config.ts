ccc664ed3e854d6bbb64534573dbba65c3cbe558
Koniec <d3ntka@gmail.com>
2023-10-14 22:26:19 +0200

    add components, pages, content type

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,10 +3,11 @@ import {z, defineCollection} from "astro:content";
 const projectsCollection = defineCollection({
     type: 'content',
     schema: z.object({
+        active: z.boolean(),
         title: z.string(),
         date: z.date(),
         description: z.string(),
-        urls: z.object({
+        links: z.object({
             github: z.string(),
             prod: z.string(),
             dev: z.string(),
@@ -19,6 +20,23 @@ const projectsCollection = defineCollection({
     })
 });
 
+
+const jobsCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        active: z.boolean(),
+        position: z.string(),
+        employer: z.string(),
+        startDate: z.date(),
+        endDate: z.date().optional,
+        present: z.boolean(),
+        description: z.string(),
+        tags: z.array(z.string()),
+    })
+});
+
+
 export const collections = {
     projects: projectsCollection,
+    jobs: jobsCollection,
 }
